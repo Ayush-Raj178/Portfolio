87bffa7b5fde75d45c1ce459af844de3ec463701
Ayush-Raj178 <ayushraj12121212@gmail.com>
2025-08-05 14:13:41 +0530

    Fix  issues

--- backend/target/generated-sources/annotations/com/portfolio/mapper/ContactMessageMapperImpl.java
+++ backend/target/generated-sources/annotations/com/portfolio/mapper/ContactMessageMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-08-05T13:47:12+0530",
+    date = "2025-08-05T14:13:08+0530",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.14 (Eclipse Adoptium)"
 )
 @Component
