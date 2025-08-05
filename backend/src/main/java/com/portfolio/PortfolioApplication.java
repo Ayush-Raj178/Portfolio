95145e3730074508f7205db6c7789e77dae7b6b3
Ayush-Raj178 <ayushraj12121212@gmail.com>
2025-08-05 09:31:04 +0530

    Fix Update

--- backend/src/main/java/com/portfolio/PortfolioApplication.java
+++ backend/src/main/java/com/portfolio/PortfolioApplication.java
@@ -2,8 +2,15 @@ package com.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({
+    "com.portfolio.controllers",
+    "com.portfolio.services",
+    "com.portfolio.config",
+    "com.portfolio.repositories"
+})
 public class PortfolioApplication {
     public static void main(String[] args) {
         SpringApplication.run(PortfolioApplication.class, args);
